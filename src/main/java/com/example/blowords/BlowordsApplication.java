95125c4a5db9aabfba9fe7cc84de498a47012e54
jiejiejiang2004 <zhouwuqimc@163.com>
2026-02-11 20:26:54 +0800

    初始化：生成Springboot文件

---
+++ src/main/java/com/example/blowords/BlowordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.blowords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlowordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlowordsApplication.class, args);
+	}
+
+}
